31cd7cdd496e7a61a2c1515302a855fe7a5609fe
ryamcl5741 <ryamcl5741@students.ecpi.edu>
2026-05-27 07:35:29 -0400

    completed 2.4

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -2,11 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-        firstName: String,
-        lastName: String,
-        email: String,
-        username: String,
-        password: String,
-    });
+    firstName: {
+    type: String,
+    trim: true,
+    required: true
+    },
 
+
+lastName: {
+    type: String,
+    trim: true,
+    required: true
+    },
+
+
+email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true
+    },
+
+
+username: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+    },
+
+password: {
+    type: String,
+    required: true
+    },
+
+created: {
+    type: Date,
+    default: Date.now
+    }
+});
 mongoose.model('User', UserSchema);
